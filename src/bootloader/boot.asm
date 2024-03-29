5236278d136869da206202f756b03c9d6e9ca9f4
suraj <surajkadapa103@gmail.com>
2024-03-29 23:21:13 +0530

    reading from the disk

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -33,6 +33,13 @@ main:
     MOV ss,ax ;stack
 
     MOV sp,0x7C00
+
+    MOV [ebr_drive_number], dl
+    MOV ax, 1
+    MOV cl, 1
+    MOV bx, 0x7E00
+    call disk_read
+
     MOV si, os_boot_msg
     CALL print
     HLT
@@ -62,7 +69,85 @@ done_print:
     POP si
     RET
 
-os_boot_msg: DB 'OS has booted', 0x0D, 0x0A, 0
+; input: LBA index in ax
+;cx [bits 0-5]: sector number
+;cx [bits 6-15]: cylinder
+;dh: head
+lba_to_chs:
+    PUSH ax
+    PUSH dx
 
+    XOR dx,dx
+    DIV word [bdb_sectors_per_track] ;(LBA % sectors per track) + 1<- sector
+    INC dx
+    MOV cx, dx
+
+    XOR dx, dx
+    DIV word [bdb_heads]
+
+    ;head: (LBA/sectors per track) % number of heads
+    ;cylinder: (LBA / sectors per track) / number of heads
+
+    MOV dh, dl ;head
+    MOV ch,al
+    SHL ah, 6
+    OR CL, AH ;cylinder
+
+    POP ax
+    MOV dl, al
+    POP ax
+
+    RET
+
+disk_read:
+    PUSH ax
+    PUSH bx
+    PUSH cx
+    PUSH dx
+    PUSH di
+
+    call lba_to_chs
+
+    MOV ah, 02h
+    MOV di, 3 ; counter
+
+retry:
+    STC ;set the carry
+    INT 13h
+    jnc doneRead
+
+    call diskReset
+
+    DEC di
+    TEST di,di
+    JNZ retry
+
+failDiskRead:
+    MOV si, read_failure
+    CALL print
+    HLT
+    JMP halt
+
+diskReset:
+    PUSHA
+    MOV ah,0
+    STC
+    INT 13h
+    JC failDiskRead
+    POPA
+
+doneRead:
+    pop di
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+
+    ret
+
+
+
+os_boot_msg: DB 'OS has booted', 0x0D, 0x0A, 0
+read_failure: DB 'Failed to read disk!',0x0D, 0x0A, 0
 TIMES 510-($-$$) DB 0
 DW 0AA55h
